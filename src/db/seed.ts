1bdcf3645f580cba1b7334e2059e20de50405daa
Marcelo Junior <designtechti.dev@protonmail.com>
2025-06-15 20:39:09 -0300

    :rocket: Feat: Running API locally :rocket: Creates Docker container with PostgreSQL database

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -55,7 +55,7 @@ const [manager] = await db
   .insert(users)
   .values({
     name: faker.person.fullName(),
-    email: 'diego.schell.f@gmail.com',
+    email: 'designtechti.dev@proton.me',
     role: 'manager',
   })
   .returning()
